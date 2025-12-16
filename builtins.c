485a0f5d66bdab96096429cb078993926dd0e7ea
Thorgalix <lucaspodevin@sfr.fr>
2025-12-16 17:10:16 +0100

    Dernier ajout pour la tache 6

--- builtins.c
+++ builtins.c
@@ -1,16 +1,30 @@
 #include "shell.h"
 /**
-*/
+ * builtin_env - Prints the current environment
+ * @envp: array of environment variables
+ *
+ * Return: 0 on success
+ */
 int builtin_env(char **envp)
 {
-int i = 0;
-if (!envp)
-return (0);
+	int i = 0;
+	int has_shlvl = 0;
 
-while (envp[i])
-{
-printf("%s\n", envp[i]);
-i++;
-}
-return (0);
+	if (!envp)
+		return (0);
+
+	/* Check if SHLVL exists in environment */
+	while (envp[i])
+	{
+		if (strncmp(envp[i], "SHLVL=", 6) == 0)
+			has_shlvl = 1;
+		printf("%s\n", envp[i]);
+		i++;
+	}
+
+	/* If SHLVL not found, output SHLVL=0 */
+	if (!has_shlvl)
+		printf("SHLVL=0\n");
+
+	return (0);
 }
